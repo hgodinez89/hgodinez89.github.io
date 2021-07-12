e8a9bf254f1e80b5e373d32e6015e79ef512ec9c
Hanzel Godinez <hgodinez89@hotmail.com>
2021-07-11 21:45:57 -0600

    Update sw.js
    Improvements for Service Worker

--- sw.js
+++ sw.js
@@ -74,10 +74,6 @@ self.__precacheManifest = [
     "revision": "e2ee95147a95ae79a46653ac85327385"
   },
   {
-    "url": "CNAME",
-    "revision": "08ad3a0c695714811439a2c3e362b606"
-  },
-  {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-e5cb9e0c77a823b86dc2.js"
   },
   {
@@ -277,6 +273,10 @@ self.__precacheManifest = [
     "revision": "bf3118a4e9baafa4821ef154be29996c"
   }
 ].concat(self.__precacheManifest || []);
+
+// Adding by Hanzel
+self.__precacheManifest = self.__precacheManifest.map((el) => ({ ...el, 'url': `./${el.url}` }));
+
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerRoute(/(\.js$|\.css$|static\/)/, new workbox.strategies.CacheFirst(), 'GET');
@@ -367,7 +367,9 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-ed9ef136ac2a16c13a7e.js`))) {
+  
+  // Ajusts by Hanzel
+  if (!resources || !(await caches.match(`./app-ed9ef136ac2a16c13a7e.js`))) {
     return await fetch(event.request)
   }
 
@@ -380,7 +382,8 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
     }
   }
 
-  const offlineShell = `/offline-plugin-app-shell-fallback/index.html`
+  // Ajusts by Hanzel
+  const offlineShell = `./offline-plugin-app-shell-fallback/index.html`
   const offlineShellWithKey = workbox.precaching.getCacheKeyForURL(offlineShell)
   return await caches.match(offlineShellWithKey)
 })
